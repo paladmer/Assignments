c461a3634a9b641989f2fa44c17e8191bb965d3a
mpalad <48260594+paladmer@users.noreply.github.com>
2020-03-03 14:59:01 -0500

    Update q5_get.php

--- Assignments/2/q5_get.php
+++ Assignments/2/q5_get.php
@@ -17,30 +17,29 @@ Milkshake Quantity: <?php echo $_GET["milk"]; ?><br>
 Cola Quantity: <?php echo $_GET["cola"]; ?><br>
 
 <?php 
-$hamb_Quantity= (int)$_GET["hamb"];
-$milkshake_Quantity= (int)$_GET["milk"];
-$cola_Quantity= (int)$_GET["cola"];
+
+ $hamb_Quantity= $_GET["hamb"]; 
  
-//$hamb_Quantity= 2; 
 $hamb_Price= $hamb_Quantity*4.95; 
 
 //echo "ham: $hamb_Price<br>";
 //echo "ham: $hamb_Quantity<br>";
 
-//$milkshake_Quantity= 1; 
+$milkshake_Quantity= $_GET["milk"]; 
 $milkshake_Price= $milkshake_Quantity*1.95; 
 //echo "milk: $milkshake_Price<br>";
 
-//$cola_Quantity= 1; 
+$cola_Quantity= $_GET["cola"]; 
 $cola_Price= $cola_Quantity*0.85; 
 //echo "cola: $cola_Price<br>";
 
-$sales = .075*($cola_Price + $milkshake_Price + $hamb_Price);
-$pretaxTip = .016*($cola_Price + $milkshake_Price + $hamb_Price); 
 $totalPrices = $cola_Price + $milkshake_Price + $hamb_Price ; 
-//echo "totalprice: $totalPrices<br>";
-//echo "sales: $sales<br>";
-//echo "pretaxtip: $pretaxTip<br>";
+
+$sales = .075*$totalPrices;
+
+$pretaxTip = .16*$totalPrices;
+
+
 $total = $sales + $pretaxTip + $totalPrices; 
 
 echo "You spent $$total"; 
