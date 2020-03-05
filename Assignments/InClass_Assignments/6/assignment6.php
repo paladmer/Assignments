69a50eb160dd97c7792d7a0acebfd6bcbe66660f
mpalad <48260594+paladmer@users.noreply.github.com>
2020-03-04 23:29:03 -0500

    Add files via upload

---
+++ Assignments/InClass_Assignments/6/assignment6.php
@@ -0,0 +1,28 @@
+<html>
+
+<body>
+
+Font: <?php echo $_GET["font"]; ?><br>
+Font Style: <?php echo $_GET["fontstyle"]; ?><br>
+Text: <?php echo $_GET["textarea"]; ?><br>
+
+<?php 
+
+$testarea = $_GET["textarea"];
+function textareaRet($testarea){
+	return $testarea; 
+}
+
+
+$fontt = "ComicSansMS"; 
+if($_GET['font']==$fontt ){
+	echo "cow"; 
+}
+
+
+echo $testarea; 
+?>
+<a href="index.html">GoBack</a> <br>
+
+</body>
+</html>
