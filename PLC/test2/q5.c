22d85b2ab6fd2080155c6122efdc24b9e448d8a3
mpalad <48260594+paladmer@users.noreply.github.com>
2020-04-25 20:30:39 -0400

    Update and rename q5 to q5.c

--- PLC/test2/q5.c
+++ PLC/test2/q5.c
@@ -3,11 +3,10 @@
 
 int fun(int *k) {
     *k += 4;                    // *10 +=4 
-    return 3 * (*k) - 1;     // 3*(10+4)-1
+    return 3 * (*k) - 1;     // 3*(10 + 4)-1
 }
 
 void main() {
-    
     
     int i = 10, j = 10, sum1, sum2;
     sum1 = (i / 2) + fun(&i);  // 
