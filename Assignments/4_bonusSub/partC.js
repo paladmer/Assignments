c40bda4061e48d2dc9fc976c563a51ba98ae8ce2
mpalad <48260594+paladmer@users.noreply.github.com>
2020-05-03 17:01:07 -0400

    Add files via upload

---
+++ Assignments/4_bonusSub/partC.js
@@ -0,0 +1,47 @@
+
+<script>
+var firstcard;
+var secondcard;
+var match =0; 
+var clicked = 0; 
+
+var cardPiece = new Array(); 
+cardPiece[0] = new Image(); 
+cardPiece[0].src = 'A.png'; 
+
+cardPiece[1] = new Image(); 
+cardPiece[1].src = 'B.png'; 
+
+cardPiece[2] = new Image(); 
+cardPiece[2].src = 'L.png'; 
+
+cardPiece[3] = new Image(); 
+cardPiece[3].src = 'D.png'; 
+
+cardPiece[4] = new Image(); 
+cardPiece[4].src = 'E.png'; 
+
+cardPiece[5] = new Image(); 
+cardPiece[5].src = 'A.png'; 
+
+cardPiece[6] = new Image(); 
+cardPiece[6].src = 'B.png'; 
+
+cardPiece[7] = new Image(); 
+cardPiece[7].src = 'L.png';
+
+cardPiece[8] = new Image();  
+cardPiece[8].src = 'D.png'; 
+
+cardPiece[9] = new Image(); 
+cardPiece[9].src = 'E.png'; 
+
+
+
+
+
+
+
+
+
+</script>
